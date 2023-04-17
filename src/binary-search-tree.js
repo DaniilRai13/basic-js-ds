1892d74346b1140d2dc39a64c6d1e4a24e8525be
DaniilRai13 <testnet02@mail.ru>
2023-04-17 21:20:28 +0300

    feat:add some methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,21 +6,47 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(n) {
+    this.tree = n;
+    this.left = null;
+    this.right = null;
+  }
+}
 class BinarySearchTree {
+  constructor() {
+    this.main = null
+  }
+
+  insert(main, list) {
+    if (list.tree < main.tree) {
+      return (main.left === null) ? main.left = list : this.insert(main.left, list)
+    } else {
+      return (main.right === null) ? main.right = list : this.insert(main.right, list)
+    }
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.main
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let list = new Node(data)
+    if (this.main === null) {
+      this.main = list
+    } else {
+      this.insert(this.main, list);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (this.main.data === data) {
+      return true
+    } else if (this.find(data) === null) {
+      return false
+    } else {
+      return true
+    }
   }
 
   find(/* data */) {
